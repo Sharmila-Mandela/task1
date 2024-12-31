dacfc71b477ad4d5a9073bf5bc390338bc2556fa
Sharmila-Mandela <163302720+Sharmila-Mandela@users.noreply.github.com>
2024-12-31 23:51:38 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function addNewWEField(){
-    
     let newNode=document.createElement('textarea');
     newNode.classList.add('form-control');
     newNode.classList.add('weField');
@@ -8,9 +7,9 @@ function addNewWEField(){
     let weOb=document.getElementById("we");
     let weAddButtonOb=document.getElementById("weAddButton");
     weOb.insertBefore(newNode,weAddButtonOb);
-  
-  }
-  function addNewAQField(){
+}
+
+function addNewAQField(){
     let newNode=document.createElement('textarea');
     newNode.classList.add('form-control');
     newNode.classList.add('eqField');
@@ -19,9 +18,9 @@ function addNewWEField(){
     let aqOb=document.getElementById("aq");
     let aqAddButtonOb=document.getElementById("aqAddButton");
     aqOb.insertBefore(newNode,aqAddButtonOb);
-  
-  }
-  function addNewSKField(){
+}
+
+function addNewSKField(){
     let newNode=document.createElement('textarea');
     newNode.classList.add('form-control');
     newNode.classList.add('skField');
@@ -30,9 +29,9 @@ function addNewWEField(){
     let skOb=document.getElementById("sk");
     let skAddButtonOb=document.getElementById("skAddButton");
     skOb.insertBefore(newNode,skAddButtonOb);
-  
-  }
-  function addNewACField(){
+}
+
+function addNewACField(){
     let newNode=document.createElement('textarea');
     newNode.classList.add('form-control');
     newNode.classList.add('acField');
@@ -41,9 +40,9 @@ function addNewWEField(){
     let acOb=document.getElementById("ac");
     let acAddButtonOb=document.getElementById("acAddButton");
     acOb.insertBefore(newNode,acAddButtonOb);
-  
-  }
-  function addNewPJField(){
+}
+
+function addNewPJField(){
     let newNode=document.createElement('textarea');
     newNode.classList.add('form-control');
     newNode.classList.add('pjField');
@@ -52,83 +51,82 @@ function addNewWEField(){
     let pjOb=document.getElementById("pj");
     let pjAddButtonOb=document.getElementById("pjAddButton");
     pjOb.insertBefore(newNode,pjAddButtonOb);
-  
-  }
-  //generating cv
-  function generateCV()
-  {
-    let nameField=document.getElementById("nameField").value;
-    let nameT1=document.getElementById("nameT1");
-    nameT1.innerHTML=nameField;
-    //direct
-    document.getElementById("nameT2").innerHTML=nameField;
-    //contact
-    document.getElementById("contactT").innerHTML= document.getElementById("contactField").value;
-  
-    //address
-    document.getElementById("addressT").innerHTML=document.getElementById("addressField").value;
-    //fb
-  
-   document.getElementById("fbT").innerHTML=document.getElementById("fbField").value;
-   //insta
-   document.getElementById("instaT").innerHTML=document.getElementById("instaField").value;
+}
+
+// Generating CV with Validation
+function generateCV() {
+    let nameField = document.getElementById("nameField").value;
+    let nameT1 = document.getElementById("nameT1");
+    nameT1.innerHTML = nameField;
+
+    document.getElementById("nameT2").innerHTML = nameField;
     
-    //linked
-    document.getElementById("LinkedT").innerHTML=document.getElementById("LinkedField").value;
-    //objective
-    document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
-    //we
-    let wes=document.getElementsByClassName("weField");
-    let str="";
-    for(let e of wes)
-    {
-      str= str +`<li>  ${e.value} </li>`;
-    }
-    document.getElementById("weT").innerHTML=str;
-    //aq
-    let  aqs=document.getElementsByClassName("aqField");
-    let  str1="";
-    for(let e of aqs)
-    {
-      str1 +=`<li>  ${e.value} </li>`;
-    }
-    document.getElementById("aqT").innerHTML=str1;
-    let  sks=document.getElementsByClassName("skField");
-    let  str2="";
-    for(let e of sks)
-    {
-      str2+=`<li>  ${e.value} </li>`;
-    }
-    document.getElementById("skT").innerHTML=str2;
-    let  acs=document.getElementsByClassName("acField");
-    let  str3="";
-    for(let e of acs)
-    {
-      str3 +=`<li>  ${e.value} </li>`;
-    }
-    document.getElementById("acT").innerHTML=str3;
-    let  pjs=document.getElementsByClassName("pjField");
-    let  str4="";
-    for(let e of pjs)
-    {
-      str4 +=`<li>  ${e.value} </li>`;
-    }
-    document.getElementById("pjT").innerHTML=str4;
+    let contact = document.getElementById("contactField").value;
+    let email = document.getElementById("emailField").value;
 
-
-    document.getElementById("cv-form").style.display="none";
-  
-  
-    document.getElementById("cv-template").style.display="block";
-  
-  
-  }
-    //print cv
-    function printCV(){
-      window.print();
-  
+    // Email validation
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return;
     }
-  
-  
-  
-  
+
+    // Phone validation (10 digits)
+    let phonePattern =/^[0-9]{10}$/ ;
+    if (!phonePattern.test(contact)) {
+        alert("Please enter a valid 10-digit phone number.");
+        return;
+    }
+
+    document.getElementById("contactT").innerHTML = contact;
+    document.getElementById("emailT").innerHTML = email;
+
+    document.getElementById("addressT").innerHTML = document.getElementById("addressField").value;
+    document.getElementById("fbT").innerHTML = document.getElementById("fbField").value;
+    document.getElementById("instaT").innerHTML = document.getElementById("instaField").value;
+    document.getElementById("LinkedT").innerHTML = document.getElementById("LinkedField").value;
+    document.getElementById("objectiveT").innerHTML = document.getElementById("objectiveField").value;
+
+    let wes = document.getElementsByClassName("weField");
+    let str = "";
+    for (let e of wes) {
+        str += `<li>${e.value}</li>`;
+    }
+    document.getElementById("weT").innerHTML = str;
+
+    let aqs = document.getElementsByClassName("aqField");
+    let str1 = "";
+    for (let e of aqs) {
+        str1 += `<li>${e.value}</li>`;
+    }
+    document.getElementById("aqT").innerHTML = str1;
+
+    let sks = document.getElementsByClassName("skField");
+    let str2 = "";
+    for (let e of sks) {
+        str2 += `<li>${e.value}</li>`;
+    }
+    document.getElementById("skT").innerHTML = str2;
+
+    let acs = document.getElementsByClassName("acField");
+    let str3 = "";
+    for (let e of acs) {
+        str3 += `<li>${e.value}</li>`;
+    }
+    document.getElementById("acT").innerHTML = str3;
+
+    let pjs = document.getElementsByClassName("pjField");
+    let str4 = "";
+    for (let e of pjs) {
+        str4 += `<li>${e.value}</li>`;
+    }
+    document.getElementById("pjT").innerHTML = str4;
+
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block";
+}
+
+// Print CV
+function printCV() {
+    window.print();
+}
